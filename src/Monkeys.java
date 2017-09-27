8e45eb9603becb96545c47f9a6d7a900248b0bae
Rovaldy <rapplyrs23@gmail.com>
2017-09-27 16:32:46 -0400

    It lets you decide to make a monkey happy or mad with the number of bananas.

---
+++ src/Monkeys.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Monkeys {
+    public static void main (String [] args ) {
+
+
+        Scanner scnr = new Scanner(System.in);
+
+        int bananas = scnr.nextInt();
+
+            bananas = bananas - 1;
+
+            if (bananas > 1)
+                System.out.println(" Im a happy monkey with " + bananas + " bananas");
+            else
+                System.out.println(" Im a mad monkey with " + bananas + " bananas");
+
+    }
+}
